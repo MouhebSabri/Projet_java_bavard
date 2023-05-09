24029dffb02ec6e83653dcb7e0db6941eb2dff0d
Mouheb Sabri <122006215+MouhebSabri@users.noreply.github.com>
2023-05-09 16:30:20 +0200

    Delete Projet_java_bavard/src directory

--- Projet_java_bavard/src/BavardListener.java
+++
@@ -1,4 +0,0 @@
-public interface BavardListener {
-    void onBavardConnected(PapotageEvent event);
-    void onBavardDisconnected(Bavard bavard);
-}
--- Projet_java_bavard/src/PapotageListener.java
+++
@@ -1,3 +0,0 @@
-public interface PapotageListener {
-    void onPapotageReceived(PapotageEvent event);
-}
